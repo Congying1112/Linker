3473de7a869201a55cce67647298c4364b007c63
ElaineWang0624 <elainew1112@gmail.com>
2014-06-07 18:19:49 +0800

    create floder in static for Timeline

--- index.py
+++ index.py
@@ -3,10 +3,9 @@ from flask import render_template,Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('Timeline/timeline.html', name=name)
+@app.route('/timeline/')
+def timeline():
+    return render_template('Timeline/timeline.html')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8080)
+    app.run('0.0.0.0', port=5000)
